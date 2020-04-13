f31d90b809b2e7bdda4d88117ba297c0528984c6
Guorui Ma <mgr2009@msn.com>
2020-04-12 19:46:57 -0700

    Session ID issue without database (current SECRET_KEY is fake)

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, session
 import json
+import uuid
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__, static_folder="static/")
@@ -7,10 +8,13 @@ application = Flask(__name__, static_folder="static/")
 
 @application.route("/index", methods=['GET'])
 def index():
+    session['uid'] = uuid.uuid4()
+    print(session['uid'])
     return application.send_static_file("index.html")
 
 
 # run the app.
 if __name__ == "__main__":
+    application.secret_key = 'fake_KEY123'
     application.run()
 
